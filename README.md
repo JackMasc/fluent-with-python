ed19000288a315854566585d71b165ee359c8d30
Giacomo Mascher <giacomo.mascher@glassform.ai>
2025-05-24 15:58:28 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## Fluent with Python notes
+Just some personal notes and snippets of code written while studying _Fluent with Python_
---
+++ chapter-11/deleteme.py
@@ -0,0 +1,8 @@
+class Route:
+    def __init__(self, position_log):
+        self.positions = position_log
+
+
+class Classroom:
+    def __init__(self, students):
+        self.students = students
